ae0593c7cf554de0d0ea37acb8536404fa0c8b6b
parkmiso <nomiso23@gmail.com>
2022-05-03 19:20:19 +0900

    으악

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -16,6 +23,18 @@ mongoose
   .then(() => console.log("몽고db connected"))
   .catch((error) => console.log(error));
 
-app.get("/", (req, res) => res.send("ㅋㅋ"));
+app.get("/", (req, res) => res.send("ㅋㅋ안뇽"));
+
+app.post("/register", (req, res) => {
+  //client에서 회원가입 정보 받아서 데이터베이스에 저장
+
+  const user = new User(req.body);
+  user.save((error, userInfo) => {
+    if (error) return res.json({ success: false, error });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`포트${port}`));
